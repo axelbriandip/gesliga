b99d779faae621b9717f8609e56add37eab43eb8
Axel Brian Dip <axelbriandip.rg@gmail.com>
2024-07-09 06:40:06 -0300

    jsonwebtoken done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
 // import models
 const { User } = require('../models/user.model');
 
@@ -28,6 +32,10 @@ const createUser = async(req, res, next) => {
         role
     } = req.body;
     
+    // encrypt password
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     // add to table
     const newUser = await User.create({
         firstName,
@@ -39,9 +47,11 @@ const createUser = async(req, res, next) => {
         phone,
         // photo,
         email,
-        password,
+        password: hashedPassword,
         role,
     })
+
+    newUser.password = undefined;
 
     // 201 => success and a resource has been created
     res.status(201).json({
@@ -56,13 +66,16 @@ const login = async(req,res,next) => {
 
     // validate if user is active
     const user = await User.findOne({
-        where: { email, password, isActive: true }
+        where: { email, isActive: true }
     });
 
     // if users not exists or password doesn't match, send error
-    if (!user) {
+    if ( !user || !(await bcrypt.compare(password, user.password)) ) {
         res.status(400).json({ status: 'Wrong credentials!' })
     }
+
+    // remove password for response
+    user.password = undefined;
 
     res.status(200).json({
         status: 'success',
